0d73deb4316f27524b4a6ab5cc55b35f3e4a1708
Kiran-Samuel <kirankoshy41@gmail.com>
2025-09-25 16:15:05 +0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical chatbot",
+    version="0.1.0",
+    author="Kiran Koshy Samuel",
+    author_email="kirankoshy41@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
